d74a40184603340b8f300ca2cc84654e8c8271ed
J. M. Ashfiqur Rahman <anik.ku.cse@gmail.com>
2019-04-20 17:15:41 +0600

    project migrated to androidx library

--- app/src/androidTest/java/anik/cseku/andoridarechitechture/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/anik/cseku/andoridarechitechture/ExampleInstrumentedTest.java
@@ -1,8 +1,8 @@
 package anik.cseku.andoridarechitechture;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
--- app/src/main/java/anik/cseku/andoridarechitechture/NoteRepository.java
+++ app/src/main/java/anik/cseku/andoridarechitechture/NoteRepository.java
@@ -1,7 +1,7 @@
 package anik.cseku.andoridarechitechture;
 
 import android.app.Application;
-import android.arch.lifecycle.LiveData;
+import androidx.lifecycle.LiveData;
 import android.os.AsyncTask;
 
 import java.util.List;
